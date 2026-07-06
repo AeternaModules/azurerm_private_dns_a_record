aabb6fa79caf50f350b6e304b728b27fb7ca0d2c
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:14:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,32 @@ output "private_dns_a_records" {
   description = "All private_dns_a_record resources"
   value       = azurerm_private_dns_a_record.private_dns_a_records
 }
+output "private_dns_a_records_fqdn" {
+  description = "List of fqdn values across all private_dns_a_records"
+  value       = [for k, v in azurerm_private_dns_a_record.private_dns_a_records : v.fqdn]
+}
+output "private_dns_a_records_name" {
+  description = "List of name values across all private_dns_a_records"
+  value       = [for k, v in azurerm_private_dns_a_record.private_dns_a_records : v.name]
+}
+output "private_dns_a_records_records" {
+  description = "List of records values across all private_dns_a_records"
+  value       = [for k, v in azurerm_private_dns_a_record.private_dns_a_records : v.records]
+}
+output "private_dns_a_records_resource_group_name" {
+  description = "List of resource_group_name values across all private_dns_a_records"
+  value       = [for k, v in azurerm_private_dns_a_record.private_dns_a_records : v.resource_group_name]
+}
+output "private_dns_a_records_tags" {
+  description = "List of tags values across all private_dns_a_records"
+  value       = [for k, v in azurerm_private_dns_a_record.private_dns_a_records : v.tags]
+}
+output "private_dns_a_records_ttl" {
+  description = "List of ttl values across all private_dns_a_records"
+  value       = [for k, v in azurerm_private_dns_a_record.private_dns_a_records : v.ttl]
+}
+output "private_dns_a_records_zone_name" {
+  description = "List of zone_name values across all private_dns_a_records"
+  value       = [for k, v in azurerm_private_dns_a_record.private_dns_a_records : v.zone_name]
+}
 
